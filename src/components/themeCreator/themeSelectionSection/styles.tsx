9374dd9663a9a6c93e058e9c9d51831fa604d0c5
lucas-gab-ls <lucaslorencetti222@outlook.com>
2026-02-07 17:46:09 -0300

    chore: :wrench: remove custom theme creator
    Theme creator was removed to avoid future color problems and inconsistencies.

--- src/components/themeCreator/themeSelectionSection/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ThemeColorSelectionSectionContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    width: 100%;
-`;
